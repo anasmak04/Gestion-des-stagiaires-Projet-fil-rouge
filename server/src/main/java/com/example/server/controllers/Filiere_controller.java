8f2121e4717b82709ea318a7f2a48ea61b509d00
anasmak04 <anasdev8@gmail.com>
2022-09-17 17:50:34 +0200

    update back-end

--- server/src/main/java/com/example/server/controllers/Filiere_controller.java
+++ server/src/main/java/com/example/server/controllers/Filiere_controller.java
@@ -3,15 +3,19 @@ package com.example.server.controllers;
 import java.util.List;
 
 import com.example.server.entities.Filiere;
+import com.example.server.entities.Stagiaire;
 import com.example.server.services.Filiere_Service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,12 +34,14 @@ public class Filiere_controller {
 	}
 	
 	@GetMapping("/{id_Filiere}")
+	@CrossOrigin(origins = "http://localhost:4200")
 	public Filiere getAllFiliereByID(@PathVariable Long id_Filiere){
 		Filiere filiere = filiere_service.getById(id_Filiere);
 		return filiere;
 	} 
 	
 	@PostMapping()
+	@CrossOrigin(origins = "http://localhost:4200")
 	public Filiere PostFilier(@RequestBody Filiere filiere) {
 		Filiere filiere1 = filiere_service.save(filiere);
 		return filiere1;
@@ -43,8 +49,15 @@ public class Filiere_controller {
 	
 	
 	@DeleteMapping("/{id_Filiere}")
+	@CrossOrigin(origins = "http://localhost:4200")
 	public void DeleteFiliereById(@PathVariable Long id_Filiere) {
 		filiere_service.delete(id_Filiere);
+	}
+	
+	@PutMapping({ "/{id_Filiere}" })
+	public ResponseEntity<Filiere> updateAdmin(@PathVariable("id_Filiere") Long id_Filiere, @RequestBody Filiere filiere) {
+		filiere_service.update(id_Filiere, filiere);
+		return new ResponseEntity<>(filiere_service.getById(id_Filiere), HttpStatus.OK);
 	}
 	
 	
--- server/src/main/java/com/example/server/controllers/Stagiaire_controller.java
+++ server/src/main/java/com/example/server/controllers/Stagiaire_controller.java
@@ -6,17 +6,20 @@ import com.example.server.entities.Stagiaire;
 import com.example.server.services.Stagiaire_Service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("stagiaire")
 public class Stagiaire_controller {
@@ -36,13 +39,23 @@ public class Stagiaire_controller {
 	}
 	
 	@PostMapping()
+	@CrossOrigin(origins = "http://localhost:4200")
 	public Stagiaire Poststagiaire(@RequestBody Stagiaire stagiaire) {
 		Stagiaire stagaiare1 = stagiare_service.save(stagiaire);
 		return stagaiare1;
 	}  
 	
 	@DeleteMapping("/{id}")
+	@CrossOrigin(origins = "http://localhost:4200")
 	public void DeleteStagiaire(@PathVariable Long id) {
 		stagiare_service.delete(id);
 	}
+	
+	
+	@PutMapping({ "/{id}" })
+	public ResponseEntity<Stagiaire> updateAdmin(@PathVariable("id") Long id, @RequestBody Stagiaire stagiaire) {
+		stagiare_service.update(id, stagiaire);
+		return new ResponseEntity<>(stagiare_service.getById(id), HttpStatus.OK);
+	}
+	
 }
